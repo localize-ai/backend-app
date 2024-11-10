3542163b384adfd6f461a8c9c1870acb580df402
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-10 18:52:25 +0700

    feat: enhance application bootstrap with Firebase initialization, global prefix, versioning, validation, CORS, and Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { firebaseSecret } from './core/firebase/firebase-secret';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerConfig } from './core/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Firebase
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseSecret),
+  });
+
+  // Global Prefix
+  app.setGlobalPrefix('api');
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // Validation
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // CORS
+  app.enableCors({
+    origin: '*',
+  });
+
+  // Swagger configuration
+  SwaggerConfig.config(app);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
