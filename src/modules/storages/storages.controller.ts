d0aae25e5afcf096ccb20e139805f1b433cf83c5
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-11 19:14:05 +0700

    feat: enable JWT authentication guard for presigned URL endpoint in storages controller

--- src/modules/storages/storages.controller.ts
+++ src/modules/storages/storages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { StoragesService } from './storages.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guard/jwtAuth.guard';
@@ -12,7 +12,7 @@ export class StoragesController {
     private readonly storagesService: StoragesService,
   ) { }
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @Get('presign')
   async getPresign(@Query() dto: UploadDto) {
