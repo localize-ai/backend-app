88dc69c9e11c5dcb93ec19d3f319851c04e3f9e7
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-13 10:15:10 +0700

    refactor: update cache key format for explores places endpoint

--- src/modules/places/places.controller.ts
+++ src/modules/places/places.controller.ts
@@ -27,7 +27,7 @@ export class PlacesController {
   }
 
   @Get('explores')
-  @CacheKey('explores-places')
+  @CacheKey('explores_places')
   @CacheTTL(1000 * 60 * 60 * 24)
   @UseInterceptors(CacheInterceptor)
   @ApiTags('places')
